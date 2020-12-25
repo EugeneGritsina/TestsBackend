8ac6ed936d770af9f02d6fcbd8eb7d76b4584161
Ilya Lazarev <ilya@it-yes.com>
2020-12-25 12:43:23 -0800

    (feature): get all subjects if pass no params to get request

--- Controllers/SubjectsController.cs
+++ Controllers/SubjectsController.cs
@@ -41,5 +41,33 @@ namespace WebApiAttempt1.Controllers
                     }).Skip(itemsAmount * (pageNumber - 1)).Take(itemsAmount).ToList();
         }
 
+        [HttpGet]
+        [Produces("application/json")]
+        public List<SubjectsListDTO> GetSubjects()
+        {
+            return (from s in TestsContext.Subjects
+                    select new SubjectsListDTO
+                    {
+                        Id = s.Id,
+                        SubjectType = (from subjectType in TestsContext.SubjectTypes
+                                       where subjectType.Id == s.SubjectTypeId
+                                       select subjectType).First(),
+                        Name = s.Name,
+                        Tests = (from t in TestsContext.Tests
+                                 where t.SubjectId == s.Id
+                                 select new TestDTO
+                                 {
+                                     Id = t.Id,
+                                     Name = t.Name,
+                                     DueDateTime = t.DueDateTime,
+                                     EstimatedTime = t.EstimatedTime,
+                                     QuestionsAmount = t.QuestionsAmount,
+                                     MaxMark = t.MaxMark,
+                                     IsOpen = t.IsOpen,
+                                     CreationDate = t.CreationDate
+                                 }).ToList()
+                    }).ToList();
+        }
+
     }
 }
